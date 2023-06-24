b7194b3f629347ed89bf9795ffb838d08947b026
2rlo <wh0776283@gmail.com>
2023-06-24 15:58:32 +0900

    add:> programmers 폰켓몬 (cpp, js)

---
+++ JS/Programmers/Level1/00 폰켓몬.js
@@ -0,0 +1,12 @@
+// Programmers 폰켓몬
+
+function solution(nums) {
+    var answer = 0;
+
+    const set = new Set(nums);
+
+    if(set.size>=nums.length/2)
+        answer = nums.length/2;
+    else answer = set.size;    
+    return answer;
+}
